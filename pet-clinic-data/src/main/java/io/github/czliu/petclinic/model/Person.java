257b5a42167cae7b9b09fb3bcc8797c19c3fe1eb
Alex Liu <liucongzhi@gmail.com>
2020-04-06 22:36:05 -0400

    add BaseEntity. close #10

--- pet-clinic-data/src/main/java/io/github/czliu/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/io/github/czliu/petclinic/model/Person.java
@@ -1,6 +1,6 @@
 package io.github.czliu.petclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/io/github/czliu/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/io/github/czliu/petclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package io.github.czliu.petclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 

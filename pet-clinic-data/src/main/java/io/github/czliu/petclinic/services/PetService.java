2e1278abd7a648799bb866051e703dc003ce0b64
Alex Liu <liucongzhi@gmail.com>
2020-04-06 22:25:51 -0400

    implement interfaces for services. close #5

---
+++ pet-clinic-data/src/main/java/io/github/czliu/petclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package io.github.czliu.petclinic.services;
+
+import io.github.czliu.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}

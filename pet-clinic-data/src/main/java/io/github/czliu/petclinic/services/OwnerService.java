8bedbdbb573b2285840ffd1697e015114c547895
Alex Liu <liucongzhi@gmail.com>
2020-04-16 22:28:54 -0400

    refactor services to common interface. close #11

--- pet-clinic-data/src/main/java/io/github/czliu/petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/io/github/czliu/petclinic/services/OwnerService.java
@@ -2,15 +2,8 @@ package io.github.czliu.petclinic.services;
 
 import io.github.czliu.petclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
 
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 }
